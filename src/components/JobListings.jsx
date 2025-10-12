514a5bbd99eb34e3fc402c9fef320fb3759147b9
Alkis <alkis.k1972@gmail.com>
2025-10-12 22:36:33 +0300

    Correct commit after Routing and Fetching data

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -81,6 +81,7 @@ export default function JobListings({ isHome = false }) {
                                 return (
                                     <JobListing
                                         key={id}
+                                        id={id}
                                         title={title}
                                         type={type}
                                         description={description}
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -3,25 +3,24 @@ import { useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 
 export default function JobPage() {
-    // const { id } = useParams();
-    // const [job, setJob] = useState(null);
-    // const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    // useEffect(() => {
-    //     const fetchJob = async () => {
-    //         try {
-    //             const res = await fetch(`/api/jobs/${id}`);
-    //             const data = await res.json();
-    //             console.log(data);
-    //             setJob(data);
-    //         } catch (error) {
-    //             console.log('Error fetching data', error);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     };
-    //     fetchJob();
-    // }, [id]);
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const res = await fetch(`/api/jobs/${id}`);
+                const data = await res.json();
+                setJob(data);
+            } catch (error) {
+                console.log('Error fetching data', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        if (id) fetchJob();
+    }, []);
 
-    return <h1>JobPage</h1>;
+    return loading ? <Spinner /> : <h1>{job.title}</h1>;
 }
